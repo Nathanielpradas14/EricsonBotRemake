25b5f107a00dd5fdfc4c64e5963a5c65b381218d
ericson_pascua <167420722+Nathanielpradas14@users.noreply.github.com>
2024-05-27 16:57:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /*
-(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠) DEV: REJARD BENTAZAR 
+(⁠ ⁠ꈍ⁠ᴗ⁠ꈍ⁠) DEV: ERICSON PASCUA
 
 * CREDITS TO: 
 ** AIZENTEL 
@@ -12,18 +12,18 @@ const { spawn } = require("child_process");
 const path = require('path');
 const YAFB_SYSTEM = "V315.js";
 const YAFB_CORE = path.join(__dirname, YAFB_SYSTEM);
-console.log(chalk.green('[SUCCES] - OctobotRemake is Now Ready to Use'));
+console.log(chalk.green('[SUCCES] - EricsonBotRemake is Now Ready to Use'));
 function start() {
-    const main = spawn("node", [YAFB_SYSTEM], {
+    const main = spawn("node", [ERICSONFB_SYSTEM], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
     });
     main.on("close", (exitCode) => {
         if (exitCode === 0) {
-            console.log(chalk.red('YAFB MAIN SYSTEM CLOSE!'));
+            console.log(chalk.red('ERICSONFB MAIN SYSTEM CLOSE!'));
         } else if (exitCode === 1) {
-            console.log(chalk.red('YAFB MAIN SYSTEM RESTARTING PLS WAIT!'));
+            console.log(chalk.red('ERICSONFB MAIN SYSTEM RESTARTING PLS WAIT!'));
             start();
         }  else {
             console.error(`Main process exited with code ${exitCode}`);
